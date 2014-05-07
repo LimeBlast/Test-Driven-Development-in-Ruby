4872084558f2d22ee9c9bf6cbdf0e1409881bbae
Daniel Hollands <daniel@limeblast.co.uk>
2014-05-07 18:35:46 +0100

    Created outline of Team spec, along with the empty Team class.

---
+++ spec/team_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../lib/team'
+
+describe Team do
+  it 'has a name'
+  it 'has a list of players'
+  it 'is favoured if it has a celebrity in it'
+  it 'complains if there is a bad word in the name'
+end
